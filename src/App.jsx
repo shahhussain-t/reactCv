d14dcb3a6c24534d4931cf2cfab9494affa4d496
shahhussaint6 <shahhussaint6@gmail.com>
2023-06-04 12:17:54 +0500

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import './index.css'
+const App = () => {
+  return (
+
+    <>
+      <div className="main_cv">
+
+
+
+
+        <div className="head_cv">
+
+          <div>
+            <h1>Shahhussian</h1>
+            <h4>Curriculum vitae</h4>
+          </div>
+
+          <div className="profile_pic">
+            <img src="/cv.jpeg" alt=""></img>
+          </div>
+
+        </div>
+
+        <div className="summary">
+          <h1>Objective</h1>
+          <p>To get an opportunity where I can make the best of my potential and contribute to the organization's growth. Seeking a position in a company where I can launch my career and build a valuable skill set. Seeking a role in an MNC where I can upgrade my skills with time and take the company to the next level.</p>
+        </div>
+
+        <div className="eduction">
+
+          <div className="eduction_inner">
+
+
+
+            <h1>Eduction:</h1>
+
+            <div className="inner">
+
+              <h2>Matric 2015-2017:</h2>
+              <p>hayat-ul-islam public school.</p>
+
+
+              <h2>FSC 2017-2019:</h2>
+              <p>Govt National College.</p>
+
+
+              <h2>BSCS 2019-Onwards:</h2>
+              <p>Federal Urdu University karachi.</p>
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div className="skils">
+
+          <h1>Skills:</h1>
+          <ul>
+            <li>html</li>
+            <li>Css</li>
+            <li>javascript</li>
+            <li>wordpress</li>
+
+
+          </ul>
+        </div>
+
+        <div className="workExpericec">
+
+
+          <h1>Work Experince:</h1>
+
+          <div className="inner">
+
+            <h2>Step by step school </h2>
+            <p>work as computer scince teacher for 6 months</p>
+
+            <h2>Servixar 2019-contiue</h2>
+            <p>working as a tranciption at servicer</p>
+
+          </div>
+
+
+
+
+        </div>
+
+        <div className="Refernce">
+
+
+          <h1>Refernce:</h1>
+          <p>Refernce will be given upon request.</p>
+
+
+        </div>
+
+      </div>
+
+    </>
+
+
+
+  )
+}
+
+export default App
